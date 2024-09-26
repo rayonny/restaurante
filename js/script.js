c1b541d4d40596ff5d8b1d1a511f08ccc9fb1a63
rayonny <rayonnygomes18@gmail.com>
2024-09-26 19:10:20 -0300

    Merge remote-tracking branch 'val/main'
    # Conflicts:
    #	index.html

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ function adicionarAoCarrinho(nome, preco) {
   carrinho.push({ nome, preco });
   localStorage.setItem('carrinho', JSON.stringify(carrinho));
   alert(`${nome} foi adicionado ao carrinho!`);
-  // Redireciona para a página do carrinho
+  // Redirecionar para a página do carrinho
   window.location.href = "carrinho.html";
 }
 
-// Função para exibir os itens do carrinho na página do carrinho
+// Função para exibir itens do carrinho na página do carrinho
 function exibirItensCarrinho() {
   const carrinhoDiv = document.getElementById('carrinhoItens');
   const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-  carrinhoDiv.innerHTML = ''; // Limpa o conteúdo anterior
+  carrinhoDiv.innerHTML = ''; // Limpar conteúdo anterior
   carrinho.forEach((item, index) => {
     const itemDiv = document.createElement('div');
     itemDiv.className = 'carrinho-item';
@@ -22,7 +22,7 @@ function exibirItensCarrinho() {
     `;
     carrinhoDiv.appendChild(itemDiv);
   });
-  calcularTotalCarrinho(); // Atualiza o total do carrinho
+  calcularTotalCarrinho(); // Atualizar o total do carrinho
 }
 
 // Função para remover item do carrinho
@@ -33,7 +33,7 @@ function removerDoCarrinho(index) {
   exibirItensCarrinho();
 }
 
-// Função para calcular o total do carrinho
+// Função para calcular o valor total do carrinho
 function calcularTotalCarrinho() {
   const numPessoas = parseInt(document.getElementById('numPessoas').value) || 0;
   const ajudanteCozinha = parseFloat(document.getElementById('ajudanteCozinha').value);
@@ -42,89 +42,65 @@ function calcularTotalCarrinho() {
   const total = (totalItens * numPessoas) + ajudanteCozinha;
 
   document.getElementById('totalCarrinho').value = `R$${total.toFixed(2)}`;
+
+  // Salvar total no localStorage para uso posterior na página de agendamento
+  localStorage.setItem('totalCarrinho', total.toFixed(2));
 }
 
-// Função para enviar pedido via WhatsApp
+// Função para enviar o pedido via WhatsApp
 function enviarWhatsApp() {
   const telefone = '5561993133448'; // Substitua pelo número desejado
-  const numPessoas = parseInt(document.getElementById('numPessoas').value) || 0;
+  const numPessoas = parseInt(document.getElementById('numPessoas').value) || 0; // Captura o número de pessoas
   const ajudanteCozinha = document.getElementById('ajudanteCozinha').value == "150" ? "Sim" : "Não";
-  const mensagem = `Olá, gostaria de fazer o seguinte pedido:\n\n${(JSON.parse(localStorage.getItem('carrinho')) || []).map(item => `${item.nome}: R$${item.preco.toFixed(2)}`).join('\n')}\n\nNúmero de Pessoas: ${numPessoas}\nAjudante na Cozinha: ${ajudanteCozinha}\n\nTotal: R$${document.getElementById('totalCarrinho').value}`;
+  const pratosSelecionados = (JSON.parse(localStorage.getItem('carrinho')) || []).map(item => `${item.nome}: R$${item.preco.toFixed(2)}`).join('\n');
+
+  const mensagem = `Olá, gostaria de fazer o seguinte pedido:\n\n${pratosSelecionados}\n\nNúmero de Pessoas: ${numPessoas}\nAjudante na Cozinha: ${ajudanteCozinha}\n\nTotal: ${document.getElementById('totalCarrinho').value}`;
+
   const url = `https://api.whatsapp.com/send?phone=${telefone}&text=${encodeURIComponent(mensagem)}`;
   window.open(url, '_blank');
 }
 
-// Inicializar carrinho na página do carrinho
+// Inicializa o carrinho na página do carrinho
 if (window.location.pathname.endsWith('carrinho.html')) {
   document.addEventListener('DOMContentLoaded', function() {
     exibirItensCarrinho();
   });
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  const agendamentoForm = document.getElementById('agendamentoForm');
+  if (agendamentoForm) {
+    agendamentoForm.addEventListener('submit', function(event) {
+      event.preventDefault();
 
-// Funções adicionais para cálculo de custo e quantidade de itens para eventos
+      const nome = document.getElementById('nome').value;
+      const email = document.getElementById('email').value;
+      const telefone = document.getElementById('telefone').value;
+      const endereco = document.getElementById('endereco').value;
+      const localizacao = document.getElementById('localizacao').value;
+      const data = document.getElementById('data').value;
+      const hora = document.getElementById('hora').value;
+      const mensagem = document.getElementById('mensagem').value;
+      const numPessoas = parseInt(document.getElementById('numPessoas').value) || 0; // Captura a quantidade de pessoas
 
-function calculateTotal() {
-  const menuPrice = parseFloat(document.querySelector('input[name="menu"]:checked').value);
-  const numberOfPeople = parseInt(document.getElementById('people').value);
-  const assistantPrice = parseFloat(document.getElementById('assistant').value);
+      const totalCarrinho = localStorage.getItem('totalCarrinho') || '0.00';
+      const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
 
-  console.log('Menu Price:', menuPrice);
-  console.log('Number of People:', numberOfPeople);
-  console.log('Assistant Price:', assistantPrice);
+      // Verifica se o carrinho contém itens
+      if (carrinho.length === 0) {
+        alert("Seu carrinho está vazio!");
+        return; // Impede o envio se o carrinho estiver vazio
+      }
 
-  if (isNaN(menuPrice) || isNaN(numberOfPeople) || isNaN(assistantPrice)) {
-    alert("Por favor, preencha todos os campos corretamente.");
-    return;
+      // Montar string com os itens do carrinho
+      const itensCarrinho = carrinho.map(item => `${item.nome}: R$${item.preco.toFixed(2)}`).join('\n');
+
+      // Criar mensagem do WhatsApp incluindo total do carrinho, itens e quantidade de pessoas
+      const message = `*Agendamento*\nNome: ${nome}\nEmail: ${email}\nTelefone: ${telefone}\nEndereço: ${endereco}\nLocalização: ${localizacao}\nData: ${data}\nHora: ${hora}\nNúmero de Pessoas: ${numPessoas}\nMensagem: ${mensagem}\n\n*Pratos Selecionados:*\n${itensCarrinho}\nTotal do Carrinho: R$${totalCarrinho}`;
+
+      const whatsappURL = `https://api.whatsapp.com/send?phone=5561993133448&text=${encodeURIComponent(message)}`;
+
+      window.open(whatsappURL, '_blank');
+    });
   }
-
-  const totalPrice = (menuPrice * numberOfPeople) + assistantPrice;
-
-  console.log('Total Price:', totalPrice);
-
-  document.getElementById('total-value').textContent = totalPrice.toFixed(2);
-}
-
-function calcular() {
-  const homens = parseInt(document.getElementById('homens').value);
-  const mulheres = parseInt(document.getElementById('mulheres').value);
-  const criancas = parseInt(document.getElementById('criancas').value);
-
-  // Quantidades médias por pessoa em gramas/ml
-  const carnePorHomem = 500; // 500g de carne bovina por homem
-  const carnePorMulher = 300; // 300g de carne bovina por mulher
-  const carnePorCrianca = 200; // 200g de carne bovina por criança
-  const frangoPorPessoa = 200; // 200g de frango por pessoa
-  const linguicaPorPessoa = 200; // 200g de linguiça por pessoa
-  const refrigerantePorMulher = 400; // 400ml de refrigerante por mulher
-  const refrigerantePorHomem = 300; // 300ml de refrigerante por homem
-  const refrigerantePorCrianca = 200; // 200ml de refrigerante por criança
-  const cervejaPorHomem = 800; // 800ml de cerveja por homem
-  const cervejaPorMulher = 500; // 500ml de cerveja por mulher
-  const aguaSucoPorPessoa = 500; // 500ml de água/suco por pessoa
-
-  // Calcular a quantidade total de carnes
-  const carneBovina = (homens * carnePorHomem) + (mulheres * carnePorMulher) + (criancas * carnePorCrianca);
-  const frango = (homens * frangoPorPessoa) + (mulheres * frangoPorPessoa) + (criancas * frangoPorPessoa);
-  const linguica = (homens * linguicaPorPessoa) + (mulheres * linguicaPorPessoa) + (criancas * linguicaPorPessoa);
-
-  // Calcular a quantidade de bebidas com base na seleção
-  let totalBebidas = 0;
-  const tipoBebida = document.getElementById('bebidas').value;
-
-  if (tipoBebida === 'refrigerante') {
-    totalBebidas = (homens * refrigerantePorHomem) + (mulheres * refrigerantePorMulher) + (criancas * refrigerantePorCrianca);
-  } else if (tipoBebida === 'cerveja') {
-    totalBebidas = (homens * cervejaPorHomem) + (mulheres * cervejaPorMulher);
-  } else {
-    totalBebidas = (homens * aguaSucoPorPessoa) + (mulheres * aguaSucoPorPessoa) + (criancas * aguaSucoPorPessoa);
-  }
-
-  // Exibir os resultados
-  document.getElementById('totalCarne').innerText = carneBovina;
-  document.getElementById('totalFrango').innerText = frango;
-  document.getElementById('totalLinguica').innerText = linguica;
-  document.getElementById('totalBebidas').innerText = totalBebidas;
-
-  document.getElementById('resultados').style.display = 'block';
-}
+});
